fd6741bcf6a5cff30f84c811f6694b1bbfc730e9
shurlow <scottyhurlow@gmail.com>
2016-04-13 13:06:42 -0500

    old school arrow + pointer

--- testtwo.js
+++ testtwo.js
@@ -8,27 +8,32 @@ Two.Resoultion = 32;
 
 var delta = new Two.Vector();
 var mouse = new Two.Vector();
-var radius = 250;
+var radius = 300;
 var centerX = two.width / 2
 var centerY = two.height / 2
+var offwhite = 'rgb(235, 235, 235)'
+var fillcolor = 'rgba(200, 200, 200, 0.5)'
+// var fillcolor = 'white'
 console.log(centerX, centerY)
 
 //Outer cirlce & shadow
 var outerShadow = two.makeCircle(centerX, centerY, radius);
 outerShadow.noStroke()
-outerShadow.fill = 'rgb(190,190,190)'
+outerShadow.fill = fillcolor
+
+console.log(outerShadow)
 
 var circle = two.makeCircle(centerX, centerY, radius);
 circle.noStroke()
-circle.fill = 'white'
+circle.fill = offwhite
 
-var innerShadow = two.makeCircle(centerX, centerY, radius - 25);
+var innerShadow = two.makeCircle(centerX, centerY, radius - 50);
 innerShadow.noStroke()
-innerShadow.fill = 'rgb(190,190,190)'
+innerShadow.fill = fillcolor
 
-var circle = two.makeCircle(centerX, centerY, radius - 25);
+var circle = two.makeCircle(centerX, centerY, radius - 50);
 circle.noStroke()
-circle.fill = 'white'
+circle.fill = offwhite
 // ball.noStroke().fill = 'blue';
 // shadow.noStroke().fill = 'rgba(5, 5, 5, 0.2)';
 // shadow.offset = new Two.Vector(10, 20);
@@ -38,7 +43,7 @@ two.bind('update', function() {
   // console.log(document.body.scrollTop)
   var new_coords = scrollToCoords()
   outerShadow.translation.set(centerX + new_coords.x, centerY + new_coords.y)
-  innerShadow.translation.set(centerX + new_coords.y, centerY + new_coords.x)
+  innerShadow.translation.set(centerX - new_coords.y, centerY - new_coords.x)
 });
 
 function scrollToCoords() {
@@ -54,6 +59,6 @@ function scrollToCoords() {
   }
 }
 
-console.log(shadow.translation.x)
+// console.log(shadow.translation.x)
 // var new_coords = scrollToCoords()
 // console.log(new_coords)
